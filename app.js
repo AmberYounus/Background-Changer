713568a6474864956edf08927dbf2468cce259db
Amber Younus <amberyounus22@gmail.com>
2023-08-10 23:59:18 +0500

    Background Changer

---
+++ app.js
@@ -0,0 +1,24 @@
+const change = document.getElementById("change");
+
+const color = document.getElementById("color");
+
+const body = document.body;
+
+change.addEventListener("click",changeBackground
+);
+
+function changeBackground() {
+
+  const color1 = getRandomRGB();
+  const color2 = getRandomRGB();
+  const color3 = getRandomRGB();
+
+  const colorString = `rgb(${color1},${color2},${color3})`;
+
+  body.style.background = colorString;
+  color.innerText = colorString;
+}
+
+function getRandomRGB() {
+  return Math.floor(Math.random() * 256);
+}
